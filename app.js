7e47a04daabef5a1b7e9468a7aed253a31901267
Simon <omojide.o.j@gmail.com>
2025-05-01 10:00:25 +0100

    Add fontawesome for delete and edit button

--- app.js
+++ app.js
@@ -186,8 +186,8 @@ const renderExpenses = (expenses) => {
       <td>${expense.category}</td>
       <td>${expense.date}</td>
       <td>
-      <button data-id="${expense.id}" class='edit-btn btn-ty1'>Edit</button>
-      <button data-id="${expense.id}" class='delete-btn'>Delete</button>
+      <button data-id="${expense.id}" class='edit-btn btn-ty1'><i class="fa-solid fa-pen"></i></button>
+      <button data-id="${expense.id}" class='delete-btn'><i class="fa-solid fa-trash"></i></button>
       </td>
     `;
     // Append row to <tbody>
